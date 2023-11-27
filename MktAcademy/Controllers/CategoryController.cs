8552fd14fe6f047fbfc92a047c9ee60f5353f3fd
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-11-27 01:15:55 +0000

    Db Context and Category table

---
+++ MktAcademy/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MktAcademy.Data;
+using MktAcademy.Models;
+
+namespace MktAcademy.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ MktAcademy/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MktAcademy.Models;
+
+namespace MktAcademy.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name="Marketing", DisplayOrder = 1},
+                new Category { Id = 2, Name = "Digital Marketing", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Legislation", DisplayOrder = 3 }
+                
+                );
+        }
+
+    }
+}
