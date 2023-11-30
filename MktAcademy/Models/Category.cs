025c071d8ae06bbfa10f4cfe12e28bda1c82cbdb
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-11-30 22:16:19 +0000

    Updates category

--- MktAcademy/Models/Category.cs
+++ MktAcademy/Models/Category.cs
@@ -10,7 +10,7 @@ namespace MktAcademy.Models
         [Required]
         [MaxLength(50)]
         [DisplayName("Category Name")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         [DisplayName("Display Order")]
         [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
