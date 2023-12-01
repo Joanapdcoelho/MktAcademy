a900c91cdd2bd604bb0a01aa84c83f518a28c549
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-01 17:55:33 +0000

    Update CategoryRepository.cs

--- MktAcademy.DataAccess/Repository/CategoryRepository.cs
+++ MktAcademy.DataAccess/Repository/CategoryRepository.cs
@@ -4,6 +4,7 @@ using MktAcademy.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,10 +13,14 @@ namespace MktAcademy.DataAccess.Repository
     public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
         private ApplicationDbContext _db;
-
         public CategoryRepository(ApplicationDbContext db) : base(db)
         {
             _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
         }
 
         public void Update(Category obj)
