c731f9d8eb7dcbd2a6bef9bbf37b2c47f6cd1e60
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-01 17:55:17 +0000

    Update IRepository.cs

--- MktAcademy.DataAccess/Repository/IRepository/IRepository.cs
+++ MktAcademy.DataAccess/Repository/IRepository/IRepository.cs
@@ -10,9 +10,11 @@ namespace MktAcademy.DataAccess.Repository.IRepository
     public interface IRepository<T> where T : class
     {
         //T - Category
+        //IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
         T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = true);
         IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
-        void Add(T entity);
+        void Add(T entity);        
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
 
