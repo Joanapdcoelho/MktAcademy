6676f11163444c1515196056b4a84b2042031ec5
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-01 17:55:40 +0000

    Update CategoryController.cs

--- MktAcademy/Controllers/CategoryController.cs
+++ MktAcademy/Controllers/CategoryController.cs
@@ -1,110 +1,108 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MktAcademy.DataAccess.Data;
+using MktAcademy.DataAccess.Repository.IRepository;
 using MktAcademy.Models;
 
 namespace MktAcademy.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db)
+        private readonly ICategoryRepository _categoryRepo;
+        public CategoryController(ICategoryRepository db)
         {
-            _db = db;
+           _categoryRepo= db;
         }
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _db.Categories.ToList();
+            List<Category> objCategoryList = _categoryRepo.GetAll().ToList();
             return View(objCategoryList);
         }
 
+        //GET Create
         public IActionResult Create()
         {
             return View();
         }
 
+        //POST Create
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            //if (obj.Name == obj.DisplayOrder.ToString())
-            //{
-            //    ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
-            //}
-
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
-                _db.SaveChanges();                
+                _categoryRepo.Add(obj);
+                _categoryRepo.Save();
                 TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
+
             return View();
 
         }
 
+        //Edit
         public IActionResult Edit(int? id)
         {
-            if(id == null || id == 0)
+            if (id == null || id == 0)
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _db.Categories.Find(id);
-            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=> u.Id == id);
-            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id ==id).FirstOrDefault();
+            Category? categoryFromDb = _categoryRepo.Get(u => u.Id == id);
+
             if (categoryFromDb == null)
             {
                 return NotFound();
             }
+
             return View(categoryFromDb);
         }
 
+
+        //POST Edit
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-
             if (ModelState.IsValid)
             {
-                _db.Categories.Update(obj);
-                _db.SaveChanges();
+                _categoryRepo.Update(obj);
+                _categoryRepo.Save();
                 TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
-            return View();
 
+            return View(obj);
         }
 
+        //Delete
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
             {
                 return NotFound();
             }
-            Category? categoryFromDb = _db.Categories.Find(id);            
+            Category categoryFromDb = _categoryRepo.Get(u => u.Id == id);
+
             if (categoryFromDb == null)
             {
                 return NotFound();
             }
+
             return View(categoryFromDb);
         }
 
+        //POST Delete
         [HttpPost, ActionName("Delete")]
         public IActionResult DeletePOST(int? id)
         {
-            Category? obj = _db.Categories.Find(id); 
-            
+            Category obj = _categoryRepo.Get(u => u.Id == id);
             if (obj == null)
             {
                 return NotFound();
             }
-
-            _db.Categories.Remove(obj);
-            _db.SaveChanges();
+            _categoryRepo.Remove(obj);
+            _categoryRepo.Save();
             TempData["success"] = "Category deleted successfully";
-            return RedirectToAction("Index");           
-
+            return RedirectToAction("Index");
         }
-
-
-
-
     }
 }
