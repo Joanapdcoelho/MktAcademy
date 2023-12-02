5ad2c84afb01ebd2e0c99d3b89c06e4e4a811447
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-02 15:52:42 +0000

    Create CourseRepository.cs

---
+++ MktAcademy.DataAccess/Repository/CourseRepository.cs
@@ -0,0 +1,43 @@
+﻿using MktAcademy.DataAccess.Data;
+using MktAcademy.DataAccess.Repository.IRepository;
+using MktAcademy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using MktAcademy.DataAccess.Repository;
+
+namespace MktAcademy.DataAccess.Repository
+{
+	public class CourseRepository : Repository<Course>, ICourseRepository
+	{
+		private ApplicationDbContext _db;
+		public CourseRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
+
+		public IEnumerable<Course> Get(Func<object, bool> value, string includeProperties)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Course obj)
+		{
+			//_db.Courses.Update(obj);
+
+			var objFromDb = _db.Courses.FirstOrDefault(u => u.Id == obj.Id);
+			if (objFromDb != null)
+			{
+				objFromDb.Name = obj.Name;
+				objFromDb.Description = obj.Description;
+				objFromDb.ListPrice = obj.ListPrice;
+				objFromDb.Price20 = obj.Price20;
+				objFromDb.Remarks = obj.Remarks;
+				
+			}
+		}
+	}
+}
