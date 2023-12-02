f7ad460e0dfce2c776620456eaa072c5036d3266
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-02 23:59:10 +0000

    Update ApplicationDbContext.cs

--- MktAcademy.DataAccess/Data/ApplicationDbContext.cs
+++ MktAcademy.DataAccess/Data/ApplicationDbContext.cs
@@ -7,9 +7,9 @@ namespace MktAcademy.DataAccess.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<Category> Categories { get; set; }
@@ -18,19 +18,21 @@ namespace MktAcademy.DataAccess.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
-                new Category { Id=1, Name="Marketing", DisplayOrder = 1},
+                new Category { Id = 1, Name = "Marketing", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "Digital Marketing", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "Legislation", DisplayOrder = 3 }
-                
+
                 );
 
             modelBuilder.Entity<Course>().HasData(
-                new Course { Id = 1, Name = "Marketing", Description = "Marketing course 25h", ListPrice = 540, Price20 = 432, Remarks = "" },
-                new Course { Id = 2, Name = "Digital Marketing", Description = "Marketing course 25h", ListPrice = 540, Price20 = 432, Remarks = ""},
-                new Course { Id = 3, Name = "E-mail Marketing", Description = "Marketing course 25h", ListPrice = 540, Price20 = 432, Remarks = ""},
-                new Course { Id = 4, Name = "Social Media Marketing", Description = "Marketing course 25h", ListPrice = 540, Price20 = 432, Remarks = ""},
-                new Course { Id = 5, Name = "Legislation 2.0", Description = "Marketing course 25h", ListPrice = 540, Price20 = 432, Remarks = ""}
+                new Course { Id = 1, Name = "Marketing", Description = "Marketing course 25h", ListPrice = 540, Price20 = 432, Remarks = "", CategoryId = 1, ImageUrl = "" },
+                new Course { Id = 2, Name = "Digital Marketing", Description = "Marketing course 25h", ListPrice = 540, Price20 = 432, Remarks = "", CategoryId = 2, ImageUrl = "" },
+                new Course { Id = 3, Name = "E-mail Marketing", Description = "Marketing course 25h", ListPrice = 540, Price20 = 432, Remarks = "", CategoryId = 3, ImageUrl = "" },
+                new Course { Id = 4, Name = "Social Media Marketing", Description = "Marketing course 25h", ListPrice = 540, Price20 = 432, Remarks = "", CategoryId = 1, ImageUrl = "" },
+                new Course { Id = 5, Name = "Legislation 2.0", Description = "Marketing course 25h", ListPrice = 540, Price20 = 432, Remarks = "", CategoryId = 2, ImageUrl = "" }
                 );
+
+            
         }
 
     }
