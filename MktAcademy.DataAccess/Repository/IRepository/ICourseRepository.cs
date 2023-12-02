d6c864a74bfeec7b17d134dbd70e3e6d4fff2eba
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-02 15:52:38 +0000

    Create ICourseRepository.cs

---
+++ MktAcademy.DataAccess/Repository/IRepository/ICourseRepository.cs
@@ -0,0 +1,16 @@
+﻿using MktAcademy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MktAcademy.DataAccess.Repository.IRepository
+{
+	public interface ICourseRepository : IRepository<Course>
+	{
+		IEnumerable<Course> Get(Func<object, bool> value, string includeProperties);
+		void Update(Course obj);
+		
+	}
+}
