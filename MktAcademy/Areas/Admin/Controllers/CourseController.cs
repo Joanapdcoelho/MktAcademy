ebdf2405a121cceebf7e200840867a1a4963e502
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-02 15:52:04 +0000

    Create CourseController.cs

---
+++ MktAcademy/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MktAcademy.DataAccess.Data;
+using MktAcademy.DataAccess.Repository.IRepository;
+using MktAcademy.Models;
+
+namespace MktAcademy.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CourseController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CourseController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Course> objCourseList = _unitOfWork.Course.GetAll().ToList();
+            return View(objCourseList);
+        }
+
+        //GET Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST Create
+        [HttpPost]
+        public IActionResult Create(Course obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Course.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        //Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Course? courseFromDb = _unitOfWork.Course.Get(u => u.Id == id);
+
+            if (courseFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(courseFromDb);
+        }
+
+
+        //POST Edit
+        [HttpPost]
+        public IActionResult Edit(Course obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Course.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Course updated successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        //Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Course categoryFromDb = _unitOfWork.Course.Get(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST Delete
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Course obj = _unitOfWork.Course.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Course.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Course deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
