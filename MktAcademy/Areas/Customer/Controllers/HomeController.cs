2921b50251bda40fa7c3c026ae7add424af9f584
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-03 02:46:31 +0000

    Update HomeController.cs

--- MktAcademy/Areas/Customer/Controllers/HomeController.cs
+++ MktAcademy/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MktAcademy.DataAccess.Repository.IRepository;
 using MktAcademy.Models;
 using System.Diagnostics;
 
@@ -8,15 +9,18 @@ namespace MktAcademy.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Course> courseList = _unitOfWork.Course.GetAll(includeProperties: "Category");
+            return View(courseList);
         }
 
         public IActionResult Privacy()
