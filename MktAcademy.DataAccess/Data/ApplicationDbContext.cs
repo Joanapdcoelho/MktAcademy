6dca62038628c5632b9854530f9692a4fa143c92
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-05 18:03:31 +0000

    Update ApplicationDbContext.cs

--- MktAcademy.DataAccess/Data/ApplicationDbContext.cs
+++ MktAcademy.DataAccess/Data/ApplicationDbContext.cs
@@ -16,6 +16,7 @@ namespace MktAcademy.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Course> Courses { get; set; }
+        public DbSet<ApplicationUser> applicationUsers { get; set; } 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
