41e8268923175087a3ebf7fccbc0fa3f91c4c3ea
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-05 02:31:37 +0000

    Update IRepository.cs

--- MktAcademy.DataAccess/Repository/IRepository/IRepository.cs
+++ MktAcademy.DataAccess/Repository/IRepository/IRepository.cs
@@ -11,7 +11,7 @@ namespace MktAcademy.DataAccess.Repository.IRepository
     {
         //T - Category
         //IEnumerable<T> GetAll();
-        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false);
         T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = true);
         IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
         void Add(T entity);        
