838b810a825725370c3841765c86f303c9371b0b
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-06 00:51:28 +0000

    Update SD.cs

--- MktAcademy.Utility/SD.cs
+++ MktAcademy.Utility/SD.cs
@@ -8,5 +8,11 @@ namespace MktAcademy.Utility
 {
     public static class SD
     {
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+        public const string Role_Student = "Student";
+        public const string Role_Instructor = "Instructor";
     }
 }
