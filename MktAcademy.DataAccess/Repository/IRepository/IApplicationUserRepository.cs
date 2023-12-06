425cfaa85f601fdf89dd4b5f930b98b2b90ae1ec
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-06 23:03:00 +0000

    Create IApplicationUserRepository.cs

---
+++ MktAcademy.DataAccess/Repository/IRepository/IApplicationUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using MktAcademy.Models;
+using MktAcademy.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MktAcademy.DataAccess.Repository.IRepository
+{
+    public interface IApplicationUserRepository : IRepository<ApplicationUser>
+    {
+        
+    }
+}
