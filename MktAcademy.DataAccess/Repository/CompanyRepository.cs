e4f5dd81497f62b1b2d0f99acecca4b836a0b71e
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-06 23:03:02 +0000

    Create CompanyRepository.cs

---
+++ MktAcademy.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,27 @@
+﻿using MktAcademy.DataAccess.Data;
+using MktAcademy.DataAccess.Repository.IRepository;
+using MktAcademy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MktAcademy.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDbContext _db;
+
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+
+        public void Update(Company obj)
+        {
+            _db.Companies.Update(obj);
+        }
+    }
+}
