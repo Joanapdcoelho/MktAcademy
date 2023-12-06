8a46ddccbaf06c9f1a918d1dd4c911223c716662
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-06 23:03:06 +0000

    Create ApplicationUserRepository.cs

---
+++ MktAcademy.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,22 @@
+﻿using MktAcademy.DataAccess.Data;
+using MktAcademy.DataAccess.Repository.IRepository;
+using MktAcademy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MktAcademy.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        
+    }
+}
