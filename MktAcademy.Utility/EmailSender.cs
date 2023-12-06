a123176182dfd8e3e7a780552baea902079af316
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-06 00:51:34 +0000

    Create EmailSender.cs

---
+++ MktAcademy.Utility/EmailSender.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MktAcademy.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //lógica do email aqui
+            return Task.CompletedTask;
+        }
+    }
+}
