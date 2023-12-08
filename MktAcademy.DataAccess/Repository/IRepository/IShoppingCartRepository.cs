7daba1edc912819eaede09bbef3cf8dec0105921
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-08 22:30:01 +0000

    Create IShoppingCartRepository.cs

---
+++ MktAcademy.DataAccess/Repository/IRepository/IShoppingCartRepository.cs
@@ -0,0 +1,19 @@
+﻿using MktAcademy.Models;
+using MktAcademy.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MktAcademy.DataAccess.Repository.IRepository
+{
+    public interface IShoppingCartRepository : IRepository<ShoppingCart>
+    {
+        int IncrementCount(ShoppingCart shoppingCart, int count);
+
+        int DecrementCount(ShoppingCart shoppingCart, int count);
+
+        void Update(ShoppingCart obj);
+    }
+}
