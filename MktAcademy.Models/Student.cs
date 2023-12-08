816e0c699eab91b00716f5ef360e2ea0a9eed483
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-08 22:29:41 +0000

    Update Student.cs

--- MktAcademy.Models/Student.cs
+++ MktAcademy.Models/Student.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +11,21 @@ namespace MktAcademy.Models
 {
     internal class Student
     {
-        
+        public int Id { get; set; }
+
+        public int CourseId { get; set; }
+
+        [ForeignKey("CourseId")]
+        [ValidateNever]
+        public Course Course { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+        public int Count { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
 
     }
 }
