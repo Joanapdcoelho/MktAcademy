b01092f49e8c4563e0ff43f5b8b8d4ca158a19fe
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-09 21:55:31 +0000

    Create OrderHeaderRepository.cs

---
+++ MktAcademy.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,26 @@
+﻿using MktAcademy.DataAccess.Data;
+using MktAcademy.DataAccess.Repository.IRepository;
+using MktAcademy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MktAcademy.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+    }
+}
