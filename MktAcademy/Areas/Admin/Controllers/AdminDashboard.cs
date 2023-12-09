d25640e7b4e3957939d7a3c80507af4ee3adbbd8
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-09 15:57:05 +0000

    Create AdminDashboard.cs

---
+++ MktAcademy/Areas/Admin/Controllers/AdminDashboard.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MktAcademy.Areas.Admin.Controllers
+{
+    public class AdminDashboard : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
