a7546082a42693fb41a97deda9b2618f4de989ff
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-09 02:22:36 +0000

    Update CartController.cs

--- MktAcademy/Areas/Customer/Controllers/CartController.cs
+++ MktAcademy/Areas/Customer/Controllers/CartController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using MktAcademy.DataAccess.Repository.IRepository;
 using MktAcademy.Models.ViewModels;
+using MktAcademy.Utility;
+using MktAcademy.Models;
+using System.Runtime.CompilerServices;
 using System.Security.Claims;
 
 namespace MktAcademy.Areas.Customer.Controllers
@@ -15,7 +18,7 @@ namespace MktAcademy.Areas.Customer.Controllers
         public CartController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
-            
+
         }
 
         public IActionResult Index()
@@ -28,7 +31,71 @@ namespace MktAcademy.Areas.Customer.Controllers
                 ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Course")
             };
 
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = GetCartPrice(cart);
+                ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+            }
+
             return View(ShoppingCartVM);
+        }
+
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+            if (cartFromDb.Count <= 1)
+            {
+                //remove from cart
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+
+        private decimal GetCartPrice(ShoppingCart shoppingCart)
+        {
+            if (User.IsInRole(SD.Role_Company) && shoppingCart.Count >= 1)
+            {
+
+                return shoppingCart.Course.Price20;
+            }
+            else
+            {
+                return shoppingCart.Course.ListPrice;
+            }
         }
     }
 }
