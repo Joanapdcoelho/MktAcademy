43f7962dc2740a94245ea4d4713e910b85544215
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-09 21:55:28 +0000

    Create OrderDetailRepository.cs

---
+++ MktAcademy.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,26 @@
+﻿using MktAcademy.DataAccess.Data;
+using MktAcademy.DataAccess.Repository.IRepository;
+using MktAcademy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MktAcademy.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private ApplicationDbContext _db;
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetails.Update(obj);
+        }
+    }
+}
