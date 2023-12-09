14968e83f6b7065f4e2cc47e3f0608bf24127152
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-09 21:55:39 +0000

    Create OrderDetail.cs

---
+++ MktAcademy.Models/OrderDetail.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MktAcademy.Models
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        [Required]
+        public int OrderHeaderId { get; set; }
+        [ForeignKey("OrderHeaderId")]
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; }
+
+        [Required]
+        public int CourseId { get; set; }
+        [ForeignKey("CourseId")]
+        [ValidateNever]
+        public Course Course { get; set; }
+
+        public int Count { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
