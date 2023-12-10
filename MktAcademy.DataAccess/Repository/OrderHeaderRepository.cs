a00a1bed77773ab2a8d9998e661da76b308abb7c
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-10 02:16:15 +0000

    Update OrderHeaderRepository.cs

--- MktAcademy.DataAccess/Repository/OrderHeaderRepository.cs
+++ MktAcademy.DataAccess/Repository/OrderHeaderRepository.cs
@@ -22,5 +22,33 @@ namespace MktAcademy.DataAccess.Repository
         {
             _db.OrderHeaders.Update(obj);
         }
+
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(u=>u.Id == id);
+            if (orderFromDb != null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if(!string.IsNullOrEmpty(paymentStatus))
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+
+            }
+        }
+
+        public void UpdateStripePaymentId(int id, string sessionId, string paymentIntendId)
+        {
+           var orderFromDb = _db.OrderHeaders.FirstOrDefault(u=> u.Id == id);
+            if(!string.IsNullOrEmpty (sessionId))
+            {
+                orderFromDb.SessionId = sessionId;
+            }
+            if(!string.IsNullOrEmpty (paymentIntendId))
+            {
+                orderFromDb.PaymentIntentId = paymentIntendId;
+                orderFromDb.PaymentDate = DateTime.Now;
+            }
+        }
     }
 }
