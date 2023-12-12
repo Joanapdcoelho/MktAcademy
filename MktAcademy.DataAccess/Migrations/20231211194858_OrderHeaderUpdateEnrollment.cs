fcb92ad53f08ea90863d86966021509b0ca0d0f4
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-12 03:23:02 +0000

    Create 20231211194858_OrderHeaderUpdateEnrollment.cs

---
+++ MktAcademy.DataAccess/Migrations/20231211194858_OrderHeaderUpdateEnrollment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MktAcademy.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderHeaderUpdateEnrollment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CourseEnrollment",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CourseEnrollment",
+                table: "OrderHeaders");
+        }
+    }
+}
