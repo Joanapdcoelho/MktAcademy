22ccc91ae5af7ef04f5bb50e175de172e41f74b8
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-12 16:53:03 +0000

    Create IInstructorRepository.cs

---
+++ MktAcademy.DataAccess/Repository/IRepository/IInstructorRepository.cs
@@ -0,0 +1,16 @@
+﻿using MktAcademy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MktAcademy.DataAccess.Repository.IRepository
+{
+	public interface IInstructorRepository : IRepository<Instructor>
+	{
+		IEnumerable<Instructor> Get(Func<object, bool> value, string includeProperties);
+		void Update(Instructor obj);
+
+	}
+}
