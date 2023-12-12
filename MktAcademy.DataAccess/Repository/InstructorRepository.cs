b9fb94b48ffa4a5978f3452bbde07128524b3f03
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-12 16:52:58 +0000

    Create InstructorRepository.cs

---
+++ MktAcademy.DataAccess/Repository/InstructorRepository.cs
@@ -0,0 +1,43 @@
+﻿using MktAcademy.DataAccess.Data;
+using MktAcademy.DataAccess.Repository.IRepository;
+using MktAcademy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using MktAcademy.DataAccess.Repository;
+
+namespace MktAcademy.DataAccess.Repository
+{
+	public class InstructorRepository : Repository<Instructor>, IInstructorRepository
+	{
+		private ApplicationDbContext _db;
+		public InstructorRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
+
+		public IEnumerable<Instructor> Get(Func<object, bool> value, string includeProperties)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Instructor obj)
+		{
+			//_db.Courses.Update(obj);
+
+			var objFromDb = _db.Instructors.FirstOrDefault(u => u.ID == obj.ID);
+			if (objFromDb != null)
+			{
+				objFromDb.FirstName = obj.FirstName;
+				objFromDb.LastName = obj.LastName;
+				objFromDb.DateOfBirth = obj.DateOfBirth;
+				objFromDb.HireDate = obj.HireDate;
+
+
+			}
+		}
+	}
+}
