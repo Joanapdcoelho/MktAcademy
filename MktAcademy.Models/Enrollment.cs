ad1295effed56d73607481b0224088f5270a09ad
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-12 16:53:20 +0000

    Create Enrollment.cs

---
+++ MktAcademy.Models/Enrollment.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MktAcademy.Models
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+        public int CourseID { get; set; }
+        [ForeignKey("CourseId")]
+        [ValidateNever]
+        public Course Course { get; set; }
+        public int StudentID { get; set; }
+        [ForeignKey("StudentId")]
+        [ValidateNever]
+        public Student Student { get; set; }
+    }
+}
