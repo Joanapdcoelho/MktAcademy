7c8195dbf0a588635e7a4f447c3ce00a193a912b
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-12 16:52:54 +0000

    Create EnrollmentRepository.cs

---
+++ MktAcademy.DataAccess/Repository/EnrollmentRepository.cs
@@ -0,0 +1,34 @@
+﻿using MktAcademy.DataAccess.Data;
+using MktAcademy.DataAccess.Repository.IRepository;
+using MktAcademy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using MktAcademy.DataAccess.Repository;
+
+namespace MktAcademy.DataAccess.Repository
+{
+	public class EnrollmentRepository : Repository<Enrollment>, IEnrollmentRepository
+	{
+		private ApplicationDbContext _db;
+		public EnrollmentRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
+
+		public IEnumerable<Enrollment> Get(Func<object, bool> value, string includeProperties)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(Enrollment obj)
+		{
+			_db.Enrollments.Update(obj);
+
+			
+		}
+	}
+}
