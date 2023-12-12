770d2710e14f1387902f5e32f1ebd28f220e01e0
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-12 03:22:43 +0000

    Update Student.cs

--- MktAcademy.Models/Student.cs
+++ MktAcademy.Models/Student.cs
@@ -9,23 +9,42 @@ using System.Threading.Tasks;
 
 namespace MktAcademy.Models
 {
-    internal class Student
+    public class Student
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = string.Empty;
 
         public int CourseId { get; set; }
 
         [ForeignKey("CourseId")]
         [ValidateNever]
         public Course Course { get; set; }
-
-        [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
-        public int Count { get; set; }
-
+        
         public string ApplicationUserId { get; set; }
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateOnly EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + ", " + LastName;
+            }
+        }
     }
 }
