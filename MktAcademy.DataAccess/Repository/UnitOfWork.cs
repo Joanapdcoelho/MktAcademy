801aa875cc2f210cd880b897a7615dee029754a4
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-12 16:53:16 +0000

    Update UnitOfWork.cs

--- MktAcademy.DataAccess/Repository/UnitOfWork.cs
+++ MktAcademy.DataAccess/Repository/UnitOfWork.cs
@@ -19,6 +19,7 @@ namespace MktAcademy.DataAccess.Repository
         public IApplicationUserRepository ApplicationUser { get; private set; }
         public IOrderDetailRepository OrderDetail { get; private set; }
         public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IStudentRepository Student { get; private set; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
@@ -29,6 +30,7 @@ namespace MktAcademy.DataAccess.Repository
             ApplicationUser = new ApplicationUserRepository(_db);
             OrderDetail = new OrderDetailRepository(_db);
             OrderHeader = new OrderHeaderRepository(_db);
+            Student = new StudentRepository(_db);
         }
 
         public void Save()
