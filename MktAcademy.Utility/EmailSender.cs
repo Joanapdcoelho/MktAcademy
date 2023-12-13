d323254d60e688edc42ed4a91882d7e8430d0e38
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-13 02:51:00 +0000

    Update EmailSender.cs

--- MktAcademy.Utility/EmailSender.cs
+++ MktAcademy.Utility/EmailSender.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MailKit.Net.Smtp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
 
 namespace MktAcademy.Utility
 {
@@ -14,7 +16,23 @@ namespace MktAcademy.Utility
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             //lógica do email aqui
+
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("mktacademy8@gmail.com"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+            //send email
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                emailClient.Authenticate("mktacademy8@gmail.com", "Mi3JLWXB!23Su");
+                emailClient.Send(emailToSend);
+                emailClient.Disconnect(true);
+            }
             return Task.CompletedTask;
+
         }
     }
 }
