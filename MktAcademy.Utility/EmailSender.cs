4aaa7222af1f03ae31df6cb18f7fd3fc1eb9452f
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-14 00:07:44 +0000

    Update EmailSender.cs

--- MktAcademy.Utility/EmailSender.cs
+++ MktAcademy.Utility/EmailSender.cs
@@ -7,7 +7,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+using System.Net.Mail;
+using System.Net;
 
 namespace MktAcademy.Utility
 {
@@ -17,22 +18,21 @@ namespace MktAcademy.Utility
         {
             //lógica do email aqui
 
-            var emailToSend = new MimeMessage();
-            emailToSend.From.Add(MailboxAddress.Parse("mktacademy8@gmail.com"));
-            emailToSend.To.Add(MailboxAddress.Parse(email));
-            emailToSend.Subject = subject;
-            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+            //var emailToSend = new MimeMessage();
+            //emailToSend.From.Add(MailboxAddress.Parse("mktacademy8@gmail.com"));
+            //emailToSend.To.Add(MailboxAddress.Parse(email));
+            //emailToSend.Subject = subject;
+            //emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
 
-            //send email
-            using (var emailClient = new SmtpClient())
-            {
-                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-                emailClient.Authenticate("mktacademy8@gmail.com", "Mi3JLWXB!23Su");
-                emailClient.Send(emailToSend);
-                emailClient.Disconnect(true);
-            }
+            ////send email
+            //using (var emailClient = new MailKit.Net.Smtp.SmtpClient())
+            //{
+            //    emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+            //    emailClient.Authenticate("mktacademy8@gmail.com", "Mi3JLWXB!23Su");
+            //    emailClient.Send(emailToSend);
+            //    emailClient.Disconnect(true);
+            //}
             return Task.CompletedTask;
-
         }
     }
 }
