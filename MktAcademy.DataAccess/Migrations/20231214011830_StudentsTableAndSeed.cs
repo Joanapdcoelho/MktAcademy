ad012c7f825fd1c6699511a817401d4370726252
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-14 01:31:12 +0000

    Create 20231214011830_StudentsTableAndSeed.cs

---
+++ MktAcademy.DataAccess/Migrations/20231214011830_StudentsTableAndSeed.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MktAcademy.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class StudentsTableAndSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_AspNetUsers_ApplicationUserId",
+                table: "Students");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Courses_CourseId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_ApplicationUserId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Grade",
+                table: "Students");
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Address", "City", "CourseId", "DateOfBirth", "EnrollmentDate", "FirstName", "ImageUrl", "LastName", "PhoneNumber", "PostalCode" },
+                values: new object[,]
+                {
+                    { 1, "123 Main Street", "Lisbon", 1, new DateOnly(1990, 5, 15), new DateOnly(2022, 9, 1), "John", "", "Doe", "123456789", "1000-001" },
+                    { 2, "456 Oak Avenue", "Porto", 1, new DateOnly(1992, 8, 20), new DateOnly(2022, 9, 1), "Jane", "", "Smith", "987654321", "2000-002" },
+                    { 3, "789 Pine Street", "Faro", 2, new DateOnly(1995, 3, 10), new DateOnly(2022, 9, 1), "Bob", "", "Johnson", "456789123", "3000-003" }
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Courses_CourseId",
+                table: "Students",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Courses_CourseId",
+                table: "Students");
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "Students",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Grade",
+                table: "Students",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_ApplicationUserId",
+                table: "Students",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_AspNetUsers_ApplicationUserId",
+                table: "Students",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Courses_CourseId",
+                table: "Students",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
