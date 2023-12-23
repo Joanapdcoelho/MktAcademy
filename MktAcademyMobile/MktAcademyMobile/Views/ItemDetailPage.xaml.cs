c18471680a6f59397bd02f632e7d94d753998ce7
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-23 11:45:28 +0000

    Created Xamarin

---
+++ MktAcademyMobile/MktAcademyMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MktAcademyMobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MktAcademyMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
