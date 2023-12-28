90695324a4a25e250f4ff694c61e0231f24a33d2
Joana Coelho <47078762+Joanapdcoelho@users.noreply.github.com>
2023-12-28 18:42:34 +0000

    added MktAcademy.Maui

---
+++ MktAcademy.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MktAcademy.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
